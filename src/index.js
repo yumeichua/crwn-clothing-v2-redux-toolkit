93f619cd3b113ad4e946edc2e9a5b89e04b880d9
Y M Chua <ymchua@Ys-MacBook-Air.local>
2023-09-28 22:54:17 +0100

    added payment method

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { Elements } from '@stripe/react-stripe-js';
 
 import App from './App';
 import { store, persistor } from './store/store';
+import { stripePromise } from './utils/stripe/stripe.utils';
 
 import './index.scss';
 
@@ -14,11 +16,13 @@ const rootElement = document.getElementById('root');
 render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
+      {/* <PersistGate loading={null} persistor={persistor}> */}
         <BrowserRouter>
-          <App />
+          <Elements stripe={ stripePromise } >
+            <App />
+          </Elements>
         </BrowserRouter>
-      </PersistGate>
+      {/* </PersistGate> */}
     </Provider>
   </React.StrictMode>,
   rootElement
